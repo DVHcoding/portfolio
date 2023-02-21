0250ad1a8becf74e01cf726700306f3a162ad9f7
Đỗ Hùng <dohung2ka3@gmail.com>
2023-02-21 16:53:39 +0700

    fix navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -97,8 +97,8 @@ const Navbar = () => {
                 <div
                     className={
                         navigation
-                            ? "fixed left-0 top-0 w-4/5 h-full bg-gradient-to-r from-blue-600 to-cyan-600 text-white p-10 ease-in duration-500"
-                            : "fixed top-0 left-[-100%] p-10 h-full ease-in duration-500"
+                            ? "fixed left-0 top-0 w-4/5 h-full bg-gradient-to-r from-blue-600 to-cyan-600 text-white p-10 transition-all ease-in duration-300"
+                            : "fixed top-0 left-[-100%] p-10 h-full ease-in-out duration-300"
                     }>
 
                     <div>
